302dbe6176dbdcd53de97a96d1c418165eeb413a
joseluetich <josefinaluetich@gmail.com>
2021-07-28 12:24:31 -0300

    Initial commit

---
+++ src/main/java/jms/dan/logistica/DanMsLogisticaApplication.java
@@ -0,0 +1,13 @@
+package jms.dan.logistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanMsLogisticaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DanMsLogisticaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jms/dan/logistica/DanMsLogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package jms.dan.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanMsLogisticaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
